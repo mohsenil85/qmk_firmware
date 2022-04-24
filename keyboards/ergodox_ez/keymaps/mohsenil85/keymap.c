6e0660dca22ee3b7c1c063488173defc5c9a9c56
Logan Mohseni <mohsenil85@gmail.com>
2022-04-24 12:12:18 -0600

    fix typo

--- keyboards/ergodox_ez/keymaps/mohsenil85/keymap.c
+++ keyboards/ergodox_ez/keymaps/mohsenil85/keymap.c
@@ -262,7 +262,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
        KC_ACL0,  LGUI(KC_C), LGUI(KC_V), LGUI(KC_T), LGUI(KC_W), LGUI(KC_L), LGUI(KC_LBRC),
-       KC_ACL1,  KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, LGUI(KC_RBRC)
+       KC_ACL1,  KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, LGUI(KC_RBRC),
                  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ESC,  KC_TRNS,
        KC_ACL2,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_MS_U, KC_TRNS,
                           KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R,
