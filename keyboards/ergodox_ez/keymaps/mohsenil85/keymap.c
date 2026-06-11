d1a65b16175ba708a8cccda60a2e21e2f204afea
Logan Mohseni <mohsenil85@gmail.com>
2026-06-11 11:25:28 -0600

    ergodox/mohsenil85: home-row mod Backspace→Escape

--- keyboards/ergodox_ez/keymaps/mohsenil85/keymap.c
+++ keyboards/ergodox_ez/keymaps/mohsenil85/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  /   |           |   '  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |Del/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |Ret/Ctrl|
+ * |Esc/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |Ret/Ctrl|
  * |--------+------+------+------+------+------|  ,   |           |   .  |------+------+------+------+------+--------|
  * | LShift |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   -  |   =  |  Up  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
         KC_GRV,         KC_1,    KC_2,     KC_3,    KC_4,   KC_5,   KC_LBRC,
         KC_TAB,         KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_SLSH,
-        CTL_T(KC_BSPC), KC_A,    KC_S,     KC_D,    KC_F,   KC_G,
+        CTL_T(KC_ESC),  KC_A,    KC_S,     KC_D,    KC_F,   KC_G,
         KC_LSFT,        KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_COMM,
         MO(META),       KC_HYPR, MO(LMIR), KC_LALT, KC_LGUI,
 	                                                 KC_ENT, KC_ESC,
